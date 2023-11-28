d0dd7dcec0093893e92325e0e7f3e9326016b8da
Michael Ryan <mbryan4@wisc.edu>
2023-11-28 11:07:35 -0600

    Shows correct class info

--- app/src/main/java/com/cs407/attendanceapp/CourseDetails.java
+++ app/src/main/java/com/cs407/attendanceapp/CourseDetails.java
@@ -1,22 +1,44 @@
 package com.cs407.attendanceapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import com.cs407.attendanceapp2.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.zxing.BarcodeFormat;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 public class CourseDetails extends AppCompatActivity {
 
+    TextView textViewCourseName;
+    TextView textViewClassDays;
+    TextView textViewClassStart;
+    TextView textViewClassEnd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +47,14 @@ public class CourseDetails extends AppCompatActivity {
         profileIcon.setOnClickListener(this::showProfilePopupMenu);
 
         String classId = getIntent().getStringExtra("classId");
+
+        // Initialize TextViews + ImageView
+        textViewCourseName = findViewById(R.id.textView21);
+        textViewClassDays = findViewById(R.id.classDaysButton);
+        textViewClassStart = findViewById(R.id.classStartText);
+        textViewClassEnd = findViewById(R.id.classEndText);
         ImageView qrCodeImageView = findViewById(R.id.qrCodeImageView);
+
 
         try {
             BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
@@ -33,6 +62,10 @@ public class CourseDetails extends AppCompatActivity {
             qrCodeImageView.setImageBitmap(bitmap);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+
+        if (classId != null) {
+            getClassDetails(classId);
         }
     }
 
@@ -55,4 +88,37 @@ public class CourseDetails extends AppCompatActivity {
         });
         popupMenu.show();
     }
+
+    private void getClassDetails(String classId) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("Classes").document(classId);
+
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot != null && documentSnapshot.exists()) {
+                    textViewCourseName.setText(documentSnapshot.getString("course_name"));
+
+                    List<String> days = (List<String>) documentSnapshot.get("days_of_week");
+                    textViewClassDays.setText("Meets On: " + TextUtils.join(", ", days));
+
+                    Date startDate = documentSnapshot.getDate("time_start");
+                    Date endDate = documentSnapshot.getDate("time_end");
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault());
+
+                    textViewClassStart.setText("Class Start Date: " + (startDate != null ? dateFormat.format(startDate) : "N/A"));
+                    textViewClassEnd.setText("Class End Date: " + (endDate != null ? dateFormat.format(endDate) : "N/A"));
+                } else {
+                    // Document does not exist
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(CourseDetails.this, "Error fetching class details", Toast.LENGTH_LONG).show();
+                Log.e("FireStoreQuery Error: ",e.getMessage());
+            }
+        });
+    }
+
 }
